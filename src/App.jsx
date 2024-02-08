36638ef0cd4ac48e33108983cdd2211b7b0a4323
SoUrAv <aansourav@gmail.com>
2024-02-08 22:09:57 +0600

    Todo Section Todo Header Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
+import TodoHeader from "./components/TodoHeader";
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
       <HeroSection />
-      <Footer/>
-    </>
+
+      {/* -----Todo Section Start----- */}
+
+      <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white mx-auto">
+        <TodoHeader />
+        <hr className="mt-4" />
+      </div>
+
+      {/* -----Todo Section End------ */}
+
+      <Footer />
+    </div>
   );
 }
 
