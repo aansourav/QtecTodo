7c7caf5ff8614c709134cbfb0946a13ddd4382be
SoUrAv <aansourav@gmail.com>
2024-02-08 23:38:25 +0600

    Redux -> Store Provider Provided to whole Application

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
+import { Provider } from "react-redux";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import TodoFooter from "./components/TodoFooter";
 import TodoHeader from "./components/TodoHeader";
 import TodoList from "./components/TodoList";
+import store from "./redux/store";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <HeroSection />
+    <Provider store={store}>
+      <div>
+        <Header />
+        <HeroSection />
 
-      {/* -----Todo Section Start----- */}
+        {/* -----Todo Section Start----- */}
 
-      <div className="sm:w-full max-w-3xl shadow-xl rounded-lg p-6  bg-white mx-6 sm:mx-auto">
-        <TodoHeader />
-        <hr className="mt-4" />
-        <TodoList />
-        <hr className="mt-4" />
-        <TodoFooter />
+        <div className="sm:w-full max-w-3xl shadow-xl rounded-lg p-6  bg-white mx-6 sm:mx-auto">
+          <TodoHeader />
+          <hr className="mt-4" />
+          <TodoList />
+          <hr className="mt-4" />
+          <TodoFooter />
+        </div>
+
+        {/* -----Todo Section End------ */}
+
+        <Footer />
       </div>
-
-      {/* -----Todo Section End------ */}
-
-      <Footer />
-    </div>
+    </Provider>
   );
 }
 
